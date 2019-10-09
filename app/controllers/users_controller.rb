6c6eb930a72558c7a58ae7635a49b6663200501c
Natalie <natsetc@gmail.com>
2019-10-09 14:26:51 -0700

    create users controller show action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,9 @@ class UsersController < ApplicationController
         users = User.all
         render json: users
     end
+
+    def show
+        user = User.find_by(id: params[:id])
+        render json: user
+    end
 end
