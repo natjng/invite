a48da278eaacdd6339da0be1a1ca8cf76ee4c55e
Natalie <natsetc@gmail.com>
2019-10-10 16:34:59 -0700

    add hosted_events, events to user serializer attrs

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,7 +1,7 @@
 class UserSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :email, :name
-  
+  attributes :email, :name, :hosted_events, :events
+
   has_many :event_users
   has_many :events, through: :event_users
   has_many :hosted_events, foreign_key: :host_id, class_name: "Event"
