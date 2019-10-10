33f330f595babd447c5160fe9f07ff74e3da94ae
Natalie <natsetc@gmail.com>
2019-10-10 16:10:21 -0700

    create event serializer

---
+++ app/serializers/event_serializer.rb
@@ -0,0 +1,8 @@
+class EventSerializer
+  include FastJsonapi::ObjectSerializer
+  attributes :name, :date, :start_time, :end_time, :location, :description, :details, :host_id
+
+  has_many :event_users
+  has_many :guests, through: :event_users, source: :user
+  belongs_to :host, class_name: "User"
+end
