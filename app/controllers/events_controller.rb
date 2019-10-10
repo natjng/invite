72c605de8031396e4e7a1e26df3f47a08cec4576
Natalie <natsetc@gmail.com>
2019-10-10 16:54:55 -0700

    create events index, show actions, use serializer

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,12 @@
 class EventsController < ApplicationController
+    def index
+        events = Event.all
+        render json: EventSerializer.new(events)
+    end
+
+    def show
+        event = Event.find_by(id: params[:id])
+        render json: EventSerializer.new(event)
+    end
+
 end
