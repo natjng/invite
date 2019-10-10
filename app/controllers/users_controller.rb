9483e2792b28a85e1122466e49883c2a31d1c9a4
Natalie <natsetc@gmail.com>
2019-10-10 16:11:11 -0700

    use user serializer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,17 @@
 class UsersController < ApplicationController
     def index
         users = User.all
-        render json: users
+        render json: UserSerializer.new(users)
     end
 
     def show
         user = User.find_by(id: params[:id])
-        render json: user
+        render json: UserSerializer.new(user)
     end
 
     def create
         user = User.find_or_create_by(user_params)
-        render json: user
+        render json: UserSerializer.new(user)
     end
 
     private
