a392183982e2b20c742fcb23afcb4d9ac4f84bf9
Natalie <natsetc@gmail.com>
2019-10-10 03:05:42 -0700

    add users create action and user_params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,15 @@ class UsersController < ApplicationController
         user = User.find_by(id: params[:id])
         render json: user
     end
+
+    def create
+        user = User.find_or_create_by(user_params)
+        render json: user
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:email)
+    end
 end
