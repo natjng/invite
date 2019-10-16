5d976eedaf4398678262af24062db6948ac178a7
Natalie <natsetc@gmail.com>
2019-10-16 15:52:10 -0700

    Add event_users attr to event serializer

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -1,6 +1,6 @@
 class EventSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :name, :date, :start_time, :end_time, :location, :description, :details, :guests, :host
+  attributes :name, :date, :start_time, :end_time, :location, :description, :details, :guests, :host, :event_users
 
   has_many :event_users
   has_many :guests, through: :event_users, source: :user
