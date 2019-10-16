666559103e3e1d877eb2c070f0807db556466221
Natalie <natsetc@gmail.com>
2019-10-16 01:40:24 -0700

    Add EventUsers index and create action

--- app/controllers/event_users_controller.rb
+++ app/controllers/event_users_controller.rb
@@ -1,2 +1,16 @@
 class EventUsersController < ApplicationController
+    def index
+        event_users = EventUser.all
+        render json: event_users
+    end
+
+    def create
+        event = Event.find_by(id: params[:event_id])
+        event_user = EventUser.new(event_id: event.id, user_id: params[:user_id])
+        if event_user.save
+            render json: EventSerializer.new(event)
+        else
+            render json: {error: "Unable to rsvp for event."}
+        end
+    end
 end
